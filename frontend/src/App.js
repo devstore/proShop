f94096b4f47fb219cf3bcb0da4cf920d3ef8b3d5
Ashutosh Singh <ashugotmail@gmail.com>
2022-01-15 15:11:20 -0800

    Added product cards with rating. Sourcing it from array, backened will be implemented later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import Header from './components/Header'
 import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <h1>Welcome to the ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer></Footer>

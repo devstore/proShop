34791aed6634b0e71b4cdc58b9efa002ec81e318
Ashutosh Singh <ashugotmail@gmail.com>
2022-02-05 23:26:22 -0800

    JWT protection middleware created and added to users/profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateTokens.js'
 
 //@desc Auth user & get Token
 //@route POST /api/users/login
@@ -9,13 +10,14 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email })
 
-  //Comparing entered password with db password
+  //Comparing entered password with db password and if true, then issuing a new jsonwebtoken(JWT)
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
-      email: user.name,
+      name: user.name,
+      email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
@@ -23,4 +25,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+//@desc Get user profile
+//@route POST /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  //Comparing entered password with db password and if true, then issuing a new jsonwebtoken(JWT)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+
+//This route will be used to protect any API we wish to protect with JWT authentication
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      //Extracting all user data from db, removing password from it and then setting to req.user
+      req.user = await User.findById(decoded.id).select('-password')
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized, token failed')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
+})
+
+export { protect }

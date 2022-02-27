e0c54e921b1c16e6d76b46e11aeaeda404b204e3
Ashutosh Singh <ashugotmail@gmail.com>
2022-02-27 11:23:09 -0800

    Added the proxy for uploads folder so that frontend can see the images loaded on server. Also added backend API for getOrders and updating orders to be marked as delivered

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,8 +4,10 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
+  updateOrderToDelivered,
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
@@ -14,9 +16,10 @@ Below shows /profile route has a get function and a put function.
 Based on the call corresponding method will be called
 */
 
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -7,4 +7,10 @@ module.exports = function (app) {
       changeOrigin: true,
     }),
   )
+  app.use(
+    createProxyMiddleware('/uploads', {
+      target: 'http://127.0.0.1:5000',
+      changeOrigin: true,
+    }),
+  )
 }
